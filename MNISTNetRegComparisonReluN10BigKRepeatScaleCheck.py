acf0727d4a9e10d79692dc6bbee6d9d22e351c7a
Sergei Volodin <sergei.volodin@epfl.ch>
2018-11-01 10:36:03 +0100

    Add results with proper init

--- MNISTNetRegComparisonReluN10BigKRepeatScaleCheck.py
+++ MNISTNetRegComparisonReluN10BigKRepeatScaleCheck.py
@@ -62,7 +62,7 @@ def get_results(pfirst = 0.5, reg_type = 'delta', reg_coeff = 1e-4, repetition =
 pfirst_options = [0.06]#np.linspace(0, 0.2, 6)[1:-1]
 reg_type_options = ['delta', 'l1', 'l2', 0]
 reg_coeff_options = [0] + list(np.logspace(-10, 0, 6))[:-1]
-repetitions = list(range(24)[worker::nProc])
+repetitions = list(range(12)[worker::nProc])
 
 print('P', pfirst_options)
 print('Reg', reg_type_options)
