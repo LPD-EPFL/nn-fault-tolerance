27e96924fdbaf3af546333f56d402d2157ca5048
Sergei Volodin <sergei.volodin@epfl.ch>
2019-05-23 19:17:04 +0200

    Include the cat to the submission

--- make_submission.sh
+++ make_submission.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf code_submission.zip
-zip code_submission.zip *.ipynb *.pkl *.py README.txt requirements.txt
+zip code_submission.zip cat.jpg *.ipynb *.pkl *.py README.txt requirements.txt
