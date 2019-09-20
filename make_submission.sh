5794029b497cc1a0c8c449d2c4a861074f2024f3
Sergei Volodin <sergei.volodin@epfl.ch>
2019-09-20 16:50:41 +0200

    Add empty figures folder

--- make_submission.sh
+++ make_submission.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf code_submission.zip
-zip code_submission.zip cat.jpg *.ipynb *.pkl *.py README.md requirements.txt
+zip code_submission.zip cat.jpg *.ipynb *.pkl *.py README.md environment.yml *.txt figures/.placeholder
